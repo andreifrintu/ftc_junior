a17ac51f3bb0a275bb7bae63983bdc38bd47d493
tb <85117454+ethical0andrew@users.noreply.github.com>
2022-12-06 19:27:43 +0200

    index

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Autonom/Autonom.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.drive.Autonom;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(group = "autonom")
+public class Autonom extends LinearOpMode
+{
+
+    SampleMecanumDrive Drive;
+
+    SimpleControls SimpleCtrl = new SimpleControls();
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        Init();
+        waitForStart();
+
+        while(!isStopRequested())
+        {
+            Trajectory tr1 = Drive.trajectoryBuilder(new Pose2d())
+                    .forward(40)
+                    .build();
+           Trajectory tr2 = Drive.trajectoryBuilder(new Pose2d(0,40))
+                    .back(40)
+                    .build();
+            Drive.followTrajectory(tr1);
+            sleep(1000);
+            Drive.followTrajectory(tr2);
+            sleep(30000);
+        }
+    }
+
+    void Init()
+    {
+
+        Drive = new SampleMecanumDrive(hardwareMap);
+        Drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Drive.setPoseEstimate(new Pose2d(0, 0));
+        Drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Autonom/SimpleControls.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.drive.Autonom;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class SimpleControls
+{
+// Drive
+
+
+    public void SetDCPos(DcMotor Lift, int pos, double power)
+    {
+        Lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Lift.setTargetPosition(pos * -1);
+        Lift.setPower(power);
+    }
+
+// Servo
+    public void SetServo(Servo servo,boolean open)
+    {
+        servo.setPosition(open ? 0.4 : 0.0);
+    }
+}
