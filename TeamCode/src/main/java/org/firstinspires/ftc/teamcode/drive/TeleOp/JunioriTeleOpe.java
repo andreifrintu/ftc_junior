fc2a1959016dcecba30d1cae6dee5c4d8a324f33
tb <85117454+ethical0andrew@users.noreply.github.com>
2022-12-08 20:52:59 +0200

    08.12.2022

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/JunioriTeleOpe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/JunioriTeleOpe.java
@@ -6,22 +6,23 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 import Utils.SimpleControls;
+
+// Robot mic
 
 @TeleOp
 public class JunioriTeleOpe extends LinearOpMode
 {
     SampleMecanumDrive mecanumDrive;
-    Gamepad gp1;
+    Gamepad gamepad1;
 
     SimpleControls simple;
 
     double suppress1 = 0.2;
 
-    boolean lr=false;
+    boolean lrSwitch = false; // ?
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -32,7 +33,7 @@ public class JunioriTeleOpe extends LinearOpMode
         while (!isStopRequested())
         {
             controlWheels();
-            Temetry();
+            Temeletry();
         }
     }
 
@@ -41,19 +42,19 @@ public class JunioriTeleOpe extends LinearOpMode
     {
         mecanumDrive = new SampleMecanumDrive(hardwareMap);
 
-        gp1 = new Gamepad();
+        gamepad1 = new Gamepad();
 
         simple = new SimpleControls();
     }
 
     void Movement()
     {
-        if (gp1.left_bumper)
-            suppress1 = 0.4;
+        if (gamepad1.left_bumper || gamepad1.right_bumper)
+            suppress1 = 0.3;
         else
-            suppress1 = 0.7;
+            suppress1 = 0.6;
 
-        Pose2d moveVec = new Pose2d(gp1.left_stick_x * suppress1, gp1.left_stick_y * suppress1);
+        Pose2d moveVec = new Pose2d(gamepad1.left_stick_x * suppress1, gamepad1.left_stick_y * suppress1);
 
         mecanumDrive.setWeightedDrivePower(moveVec);
     }
@@ -63,40 +64,39 @@ public class JunioriTeleOpe extends LinearOpMode
     {
         Pose2d poseEstimate = mecanumDrive.getPoseEstimate();
         boolean pressedStart = gamepad1.start;
-        if (pressedStart&&!lastPressedStart) lr = !lr;
+        if (pressedStart&&!lastPressedStart)
+            lrSwitch = !lrSwitch;
         lastPressedStart = pressedStart;
-        if (lr) {
-            Vector2d input = new Vector2d(
+
+        Vector2d input;
+        if (lrSwitch)
+        {
+            input = new Vector2d(
                     -gamepad1.left_stick_y*suppress1,
                     -gamepad1.left_stick_x*suppress1
             ).rotated(-poseEstimate.getHeading());
-            mecanumDrive.setWeightedDrivePower(
-                    new Pose2d(
-                            input.getX(),
-                            input.getY(),
-                            -(gamepad1.right_trigger-gamepad1.left_trigger)*suppress1
-                    )
-            );
         }
-        else {
-            Vector2d input = new Vector2d(
+        else
+        {
+            input = new Vector2d(
                     -gamepad1.right_stick_y*suppress1,
                     -gamepad1.right_stick_x*suppress1
             ).rotated(-poseEstimate.getHeading());
-            mecanumDrive.setWeightedDrivePower(
-                    new Pose2d(
-                            input.getX(),
-                            input.getY(),
-                            -(gamepad1.right_trigger-gamepad1.left_trigger)*suppress1
-                    )
-            );
         }
+
+        mecanumDrive.setWeightedDrivePower(
+                new Pose2d(
+                        input.getX(),
+                        input.getY(),
+                        -(gamepad1.right_trigger-gamepad1.left_trigger)*suppress1
+                )
+        );
     }
 
-    void Temetry()
+    void Temeletry()
     {
-        telemetry.addData("X:", gp1.left_stick_x);
-        telemetry.addData("Y:", gp1.left_stick_y);
+        telemetry.addData("X:", gamepad1.left_stick_x);
+        telemetry.addData("Y:", gamepad1.left_stick_y);
         telemetry.update();
     }
 }
