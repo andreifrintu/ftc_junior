77ec46bf369b64ae45b5d59a497cd3bea44f04a1
tb <85117454+ethical0andrew@users.noreply.github.com>
2022-12-13 21:02:34 +0200

    13.12.2022 - JunioriTeleOpe merge tzava
    amogus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/JunioriTeleOpe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/JunioriTeleOpe.java
@@ -4,9 +4,12 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import java.util.Vector;
 
 import Utils.SimpleControls;
 
@@ -16,11 +19,10 @@ import Utils.SimpleControls;
 public class JunioriTeleOpe extends LinearOpMode
 {
     SampleMecanumDrive mecanumDrive;
-    Gamepad gamepad1;
 
     SimpleControls simple;
 
-    double suppress1 = 0.2;
+    double suppress;
 
     boolean lrSwitch = false; // ?
 
@@ -28,10 +30,11 @@ public class JunioriTeleOpe extends LinearOpMode
     public void runOpMode() throws InterruptedException
     {
         Init();
-
+        waitForStart();
 
         while (!isStopRequested())
         {
+            Suppress();
             controlWheels();
             Temeletry();
         }
@@ -41,22 +44,20 @@ public class JunioriTeleOpe extends LinearOpMode
     void Init()
     {
         mecanumDrive = new SampleMecanumDrive(hardwareMap);
-
-        gamepad1 = new Gamepad();
+        mecanumDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        mecanumDrive.setPoseEstimate(new Pose2d(0, 0));
+        mecanumDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mecanumDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         simple = new SimpleControls();
     }
 
-    void Movement()
+    private void Suppress()
     {
-        if (gamepad1.left_bumper || gamepad1.right_bumper)
-            suppress1 = 0.3;
+        if (gamepad1.right_bumper)
+            suppress = 0.3;
         else
-            suppress1 = 0.6;
-
-        Pose2d moveVec = new Pose2d(gamepad1.left_stick_x * suppress1, gamepad1.left_stick_y * suppress1);
-
-        mecanumDrive.setWeightedDrivePower(moveVec);
+            suppress = 0.6;
     }
 
     boolean lastPressedStart = false;
@@ -68,29 +69,19 @@ public class JunioriTeleOpe extends LinearOpMode
             lrSwitch = !lrSwitch;
         lastPressedStart = pressedStart;
 
-        Vector2d input;
-        if (lrSwitch)
-        {
-            input = new Vector2d(
-                    -gamepad1.left_stick_y*suppress1,
-                    -gamepad1.left_stick_x*suppress1
+        Vector2d input = new Vector2d(
+                    -gamepad1.left_stick_y * suppress,
+                    -gamepad1.left_stick_x * suppress
             ).rotated(-poseEstimate.getHeading());
-        }
-        else
-        {
-            input = new Vector2d(
-                    -gamepad1.right_stick_y*suppress1,
-                    -gamepad1.right_stick_x*suppress1
-            ).rotated(-poseEstimate.getHeading());
-        }
-
-        mecanumDrive.setWeightedDrivePower(
+        mecanumDrive.setDrivePower(
                 new Pose2d(
                         input.getX(),
                         input.getY(),
-                        -(gamepad1.right_trigger-gamepad1.left_trigger)*suppress1
+                        -(gamepad1.right_trigger-gamepad1.left_trigger) * suppress
                 )
         );
+
+        telemetry.addData("mV", input);
     }
 
     void Temeletry()
